e65464762c72e004bcaca0e10348b7f8cce8f8b6
bruce <bruce@333ed61c-d966-4eaf-8115-f610d1735cec>
2012-10-17 00:38:59 +0000

    make paths relative in build script
    git-svn-id: https://svn.broadinstitute.org/gaag/pilon/trunk@3027 333ed61c-d966-4eaf-8115-f610d1735cec

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ cp -p $f $tmp
 sed -e "s/\(date.*=\).*/\\1 \"$date\"/"  -e "s/\(svn.*=\).*/\\1 \"$svn\"/" <$tmp >$f
 #sbt $* package
 sbt $* one-jar
-cp -p ~bruce/dev/gaag/sandbox/bruce/pilon/target/scala-2.9.2/pilon_2.9.2-1.0-one-jar.jar ~bruce/lib/pilon/pilon.jar
+cp -p target/scala-2.9.2/pilon_2.9.2-1.0-one-jar.jar ~/lib/pilon/pilon.jar
 mv $tmp $f
