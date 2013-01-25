9b4fc53e17ef625396dcf03a5fd8b2c440ea6ca7
bruce <bruce@333ed61c-d966-4eaf-8115-f610d1735cec>
2013-01-25 21:30:25 +0000

    debugging possible source of physical coverage issue in aplysia
    git-svn-id: https://svn.broadinstitute.org/gaag/pilon/trunk@3732 333ed61c-d966-4eaf-8115-f610d1735cec

--- src/main/scala/org/broadinstitute/pilon/PileUpRegion.scala
+++ src/main/scala/org/broadinstitute/pilon/PileUpRegion.scala
@@ -177,6 +177,7 @@ class PileUpRegion(name: String, start: Int, stop: Int)
     }
 
     readCount += 1
+    require(valid && r.getReferenceIndex == r.getMateReferenceIndex, "valid pair with reference mismatch " + r)
     physCovIncr(aStart, aEnd, insert, paired, valid)
   }
 
