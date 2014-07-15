479a47157ee2f094e83e150c903beb740d2d8c20
Bruce Walker <bruce@broadinstitute.org>
2014-07-15 15:50:55 -0400

    Update copyright notices

--- src/main/scala/org/broadinstitute/pilon/Scaffold.scala
+++ src/main/scala/org/broadinstitute/pilon/Scaffold.scala
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2012-2014 Broad Institute, Inc.
+ *
+ * This file is part of Pilon.
+ *
+ * Pilon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * Pilon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Pilon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.broadinstitute.pilon
 
 /**
